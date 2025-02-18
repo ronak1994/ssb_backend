8805d179014a22279dfbc03bb56a5701c5491c64
Ronak vaya <ronakvaya@gmail.com>
2025-02-18 11:36:04 +0530

    email otp copy btn added

--- src/templates/otpEmailTemplate.js
+++ src/templates/otpEmailTemplate.js
@@ -1,215 +1,250 @@
 const otpEmailTemplate = (otp) => {
-    return `
-   <!DOCTYPE html>
+  return `
+ <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>SSB OTP Verification</title>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>SSB OTP Verification</title>
 
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+    rel="stylesheet"
+  />
+</head>
+<body
+  style="
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+    background: #ffffff;
+    font-size: 14px;
+  "
+>
+  <div
     style="
-      margin: 0;
-      font-family: 'Poppins', sans-serif;
-      background: #ffffff;
+      max-width: 680px;
+      margin: 0 auto;
+      padding: 45px 30px 60px;
+      background: #f4f7ff;
+      background-image: url(https://theodin.in/demossb/assets/img/building.png);
+      background-repeat: no-repeat;
+      background-size: 800px 452px;
+      background-position: top center;
       font-size: 14px;
+      color: #434343;
     "
   >
-    <div
-      style="
-        max-width: 680px;
-        margin: 0 auto;
-        padding: 45px 30px 60px;
-        background: #f4f7ff;
-        background-image: url(https://theodin.in/demossb/assets/img/building.png);
-        background-repeat: no-repeat;
-        background-size: 800px 452px;
-        background-position: top center;
-        font-size: 14px;
-        color: #434343;
-      "
-    >
-      <header>
-        <table style="width: 100%;">
-          <tbody>
-            <tr style="height: 0;">
-              <td>
-                <img
-                  alt=""
-                  src="https://theodin.in/demossb/assets/img/logo.png"
-                  height="30px"
-                />
-              </td>
-              <td style="text-align: right;">
-                
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </header>
+    <header>
+      <table style="width: 100%;">
+        <tbody>
+          <tr style="height: 0;">
+            <td>
+              <img
+                alt=""
+                src="https://theodin.in/demossb/assets/img/logo.png"
+                height="30px"
+              />
+            </td>
+            <td style="text-align: right;">
+              
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </header>
 
-      <main>
-        <div
-          style="
-            margin: 0;
-            margin-top: 70px;
-            padding: 92px 30px 115px;
-            background: #ffffff;
-            border-radius: 30px;
-            text-align: center;
-          "
-        >
-          <div style="width: 100%; max-width: 489px; margin: 0 auto;">
-            <h1
+    <main>
+      <div
+        style="
+          margin: 0;
+          margin-top: 70px;
+          padding: 92px 30px 115px;
+          background: #ffffff;
+          border-radius: 30px;
+          text-align: center;
+        "
+      >
+        <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+          <h1
+            style="
+              margin: 0;
+              font-size: 24px;
+              font-weight: 500;
+              color: #1f1f1f;
+            "
+          >
+            Your OTP
+          </h1>
+          <p
+            style="
+              margin: 0;
+              margin-top: 17px;
+              font-size: 16px;
+              font-weight: 500;
+            "
+          >
+            Hey There,
+          </p>
+          <p
+            style="
+              margin: 0;
+              margin-top: 17px;
+              font-weight: 500;
+              letter-spacing: 0.56px;
+            "
+          >
+            Thank you for choosing Step Stamp. Use the following OTP
+            to complete the procedure to change your email address. OTP is
+            valid for
+            <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
+            Do not share this code with others, including SSB
+            employees.
+          </p>
+          
+          <div 
+            style="
+              margin: 0;
+              margin-top: 60px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              gap: 10px;
+            "
+          >
+            <span
+              id="otp-code"
               style="
-                margin: 0;
-                font-size: 24px;
-                font-weight: 500;
-                color: #1f1f1f;
-              "
-            >
-              Your OTP
-            </h1>
-            <p
-              style="
-                margin: 0;
-                margin-top: 17px;
-                font-size: 16px;
-                font-weight: 500;
-              "
-            >
-              Hey There,
-            </p>
-            <p
-              style="
-                margin: 0;
-                margin-top: 17px;
-                font-weight: 500;
-                letter-spacing: 0.56px;
-              "
-            >
-              Thank you for choosing Step Stamp. Use the following OTP
-              to complete the procedure to change your email address. OTP is
-              valid for
-              <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
-              Do not share this code with others, including Archisketch
-              employees.
-            </p>
-            <p
-              style="
-                margin: 0;
-                margin-top: 60px;
-                font-size: 40px;
+                font-size: 36px;
                 font-weight: 600;
-                letter-spacing: 25px;
+                letter-spacing: 15px;
                 color: #ba3d4f;
               "
             >
-            ${otp}
-            </p>
+              ${otp}
+            </span>
+            <button
+              onclick="copyOTP()"
+              style="
+                background: none;
+                border: none;
+                cursor: pointer;
+                padding: 5px;
+              "
+              title="Copy OTP"
+            >
+              <img 
+                src="https://cdn-icons-png.flaticon.com/512/724/724928.png" 
+                width="24px" 
+                alt="Copy OTP"
+              />
+            </button>
           </div>
+
         </div>
+      </div>
 
-        <p
-          style="
-            max-width: 400px;
-            margin: 0 auto;
-            margin-top: 90px;
-            text-align: center;
-            font-weight: 500;
-            color: #8c8c8c;
-          "
-        >
-          Need help? Ask at
-          <a
-            href="mailto:archisketch@gmail.com"
-            style="color: #499fb6; text-decoration: none;"
-            >contact@stepstamp.com</a
-          >
-          or visit our
-         
-        </p>
-      </main>
-
-      <footer
+      <p
         style="
-          width: 100%;
-          max-width: 490px;
-          margin: 20px auto 0;
+          max-width: 400px;
+          margin: 0 auto;
+          margin-top: 90px;
           text-align: center;
-          border-top: 1px solid #e6ebf1;
+          font-weight: 500;
+          color: #8c8c8c;
         "
       >
-        <p
-          style="
-            margin: 0;
-            margin-top: 40px;
-            font-size: 16px;
-            font-weight: 600;
-            color: #434343;
-          "
+        Need help? Ask at
+        <a
+          href="mailto:contact@stepstamp.com"
+          style="color: #499fb6; text-decoration: none;"
+          >contact@stepstamp.com</a
         >
-          Step Stamp 
-        </p>
-        <p style="margin: 0; margin-top: 8px; color: #434343;">
-          Address 540, City, State.
-        </p>
-        <div style="margin: 0; margin-top: 16px;">
-          <a href="" target="_blank" style="display: inline-block;">
-            <img
-              width="36px"
-              alt="Facebook"
-              src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook"
-            />
-          </a>
-          <a
-            href=""
-            target="_blank"
-            style="display: inline-block; margin-left: 8px;"
-          >
-            <img
-              width="36px"
-              alt="Instagram"
-              src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram"
-          /></a>
-          <a
-            href=""
-            target="_blank"
-            style="display: inline-block; margin-left: 8px;"
-          >
-            <img
-              width="36px"
-              alt="Twitter"
-              src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter"
-            />
-          </a>
-          <a
-            href=""
-            target="_blank"
-            style="display: inline-block; margin-left: 8px;"
-          >
-            <img
-              width="36px"
-              alt="Youtube"
-              src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube"
-          /></a>
-        </div>
-        <p style="margin: 0; margin-top: 16px; color: #434343;">
-          Copyright © 2025 Company. All rights reserved.
-        </p>
-      </footer>
-    </div>
-  </body>
-</html>
+      </p>
+    </main>
 
-    `;
-  };
-  
-  export default otpEmailTemplate;
-  
+    <footer
+      style="
+        width: 100%;
+        max-width: 490px;
+        margin: 20px auto 0;
+        text-align: center;
+        border-top: 1px solid #e6ebf1;
+      "
+    >
+      <p
+        style="
+          margin: 0;
+          margin-top: 40px;
+          font-size: 16px;
+          font-weight: 600;
+          color: #434343;
+        "
+      >
+        Step Stamp 
+      </p>
+      <p style="margin: 0; margin-top: 8px; color: #434343;">
+        Address 540, City, State.
+      </p>
+      <div style="margin: 0; margin-top: 16px;">
+        <a href="" target="_blank" style="display: inline-block;">
+          <img
+            width="36px"
+            alt="Facebook"
+            src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook"
+          />
+        </a>
+        <a
+          href=""
+          target="_blank"
+          style="display: inline-block; margin-left: 8px;"
+        >
+          <img
+            width="36px"
+            alt="Instagram"
+            src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram"
+        /></a>
+        <a
+          href=""
+          target="_blank"
+          style="display: inline-block; margin-left: 8px;"
+        >
+          <img
+            width="36px"
+            alt="Twitter"
+            src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter"
+          />
+        </a>
+        <a
+          href=""
+          target="_blank"
+          style="display: inline-block; margin-left: 8px;"
+        >
+          <img
+            width="36px"
+            alt="Youtube"
+            src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube"
+        /></a>
+      </div>
+      <p style="margin: 0; margin-top: 16px; color: #434343;">
+        Copyright © 2025 Company. All rights reserved.
+      </p>
+    </footer>
+  </div>
+
+  <script>
+    function copyOTP() {
+      const otpText = document.getElementById("otp-code").innerText;
+      navigator.clipboard.writeText(otpText).then(() => {
+        alert("OTP copied to clipboard!");
+      }).catch(err => {
+        console.error("Error copying OTP: ", err);
+      });
+    }
+  </script>
+</body>
+</html>
+  `;
+};
+
+export default otpEmailTemplate;
