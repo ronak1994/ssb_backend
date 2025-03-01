c49a13b940bae661ad1c11ac0acc81e4b68a7a2d
Ronak vaya <ronakvaya@gmail.com>
2025-03-01 16:44:46 +0530

    test

--- src/controllers/blockchain.controller.js
+++ src/controllers/blockchain.controller.js
@@ -31,9 +31,10 @@ const purchaseBlockchain = catchAsync(async (req, res) => {
 })
 
 const saveSwapping = catchAsync(async (req, res) => {
-  const transaction = await blockchainService.saveSwapTransaction(req.body);
- 
-  res.status(httpStatus.CREATED).json({ message: 'Transaction recorded successfully', data: transaction });
+  console.log(req.body);
+  //const transaction = await blockchainService.saveSwapTransaction(req.body);
+  
+ // res.status(httpStatus.CREATED).json({ message: 'Transaction recorded successfully', data: transaction });
 })
 
 
